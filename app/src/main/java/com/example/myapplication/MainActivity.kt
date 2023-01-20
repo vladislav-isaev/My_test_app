2565e26b712a2b98f014ee58d8a3c55ea33f240c
Vladislav Isayev <vladislav.isaev@vk.team>
2023-01-21 00:10:06 +0300

    some work

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        some work
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
